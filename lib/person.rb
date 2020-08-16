938adf2c08c77a04a80d983782120d7bff8566be
Saida Davis <saihdavis@gmail.com>
2020-08-16 06:39:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each{|key, value|}
+    attributes.each{|key, value|.send
     
   end
 
